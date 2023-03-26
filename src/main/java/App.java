cd6817e1fad546497298945fc787a839097ba1ec
DanCh11 <dan.chiriac1453@gmail.com>
2023-03-26 19:12:15 +0200

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import services.crawlers.LyricsCrawler;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
