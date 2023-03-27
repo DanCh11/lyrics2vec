c37b881280d5fb6e6bc0ee76577e1b659d99071f
DanCh11 <dan.chiriac1453@gmail.com>
2023-03-27 17:05:01 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,3 @@
-import services.crawlers.LyricsCrawler;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
