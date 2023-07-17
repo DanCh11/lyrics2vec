09cf7072b5bfe7d1d27ef4d94d262edb6bbed695
Daycu <daycu@nachspiel.fritz.box>
2023-07-17 20:40:02 +0200

    added spring boot (finally)

--- src/main/java/de/daycu/springLyrics2Vec/models/Record.java
+++ src/main/java/de/daycu/springLyrics2Vec/models/Record.java
@@ -1,20 +1,15 @@
-package models;
+package de.daycu.springLyrics2Vec.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.solr.client.solrj.beans.Field;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class Record {
-    @Field("id")
     private String id;
-    @Field("artistName")
     private String artistName;
-    @Field("songName")
     private String song;
-    @Field("lyrics")
     private String lyrics;
 }
